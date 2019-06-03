50e2334a0b3069feda112e06b85592e01a0a38b2
qiaodan <2031015269@qq.com>
2019-06-03 16:56:02 +0800

    123

---
+++ AppAnalysis/advance.py
@@ -0,0 +1,12 @@
+# 删除重复行
+def drop_duplicat_row(data):
+    for i in data.duplicated()[1:]:
+        if i:
+            print('存在重复行')
+            break
+    data.drop_duplicates(keep='first', inplace=True)
+#删除空值
+def drop_nan(data):
+    del_rows=data[data.isnull().T.any()].index.tolist()
+    for i in del_rows:
+        data.drop(labels=i, axis=0, index=None, columns=None, inplace=True)
---
+++ AppAnalysis/run.py
@@ -0,0 +1,3 @@
+import pandas as pd
+gebder_age_train_data = pd.read_csv('data\gender_age_train.csv')
+print(gebder_age_train_data.head())
