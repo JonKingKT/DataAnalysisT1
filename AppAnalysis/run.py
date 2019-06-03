9e28842f581467d66a64214bc2d9fe30b3c40d62
qiaodan <2031015269@qq.com>
2019-06-03 17:24:20 +0800

    预处理内存爆炸

--- AppAnalysis/run.py
+++ AppAnalysis/run.py
@@ -1,3 +1,20 @@
 import pandas as pd
-gebder_age_train_data = pd.read_csv('data\gender_age_train.csv')
-print(gebder_age_train_data.head())
+import advance
+# gebder_age_train_data = pd.read_csv('data\gender_age_train.csv')
+# gebder_age_test_data = pd.read_csv('data\gender_age_test.csv')
+#app_events_data = pd.read_csv('data\\app_events.csv')
+# app_labels_data = pd.read_csv('data\\app_labels.csv')
+# events_data = pd.read_csv('data\\events.csv')
+# label_categories_data = pd.read_csv('data\\label_categories.csv')
+# phone_brand_device_model_data = pd.read_csv('data\\phone_brand_device_model.csv')
+# data_list=[gebder_age_train_data,
+#            gebder_age_test_data,
+#            app_events_data,
+#            app_labels_data,
+#            events_data,
+#            label_categories_data,
+#            phone_brand_device_model_data]
+# for data1 in data_list:
+#     advance.drop_duplicat_row(data1)
+#     advance.drop_nan(data1)
+# print(events_data.head())
