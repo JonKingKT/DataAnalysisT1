84cb911dba753bcefec94e7062f98a60b038761d
qiaodan <2031015269@qq.com>
2019-06-03 17:41:11 +0800

    预处理

--- AppAnalysis/advance.py
+++ AppAnalysis/advance.py
@@ -1,3 +1,4 @@
+
 # 删除重复行
 def drop_duplicat_row(data):
     for i in data.duplicated()[1:]:
@@ -10,6 +11,8 @@ def drop_nan(data):
     del_rows=data[data.isnull().T.any()].index.tolist()
     for i in del_rows:
         data.drop(labels=i, axis=0, index=None, columns=None, inplace=True)
+#删除不需要的列
+def drop_col(data_all,del_col):
+    for columns in del_col:
+        data_all.drop(labels=columns, axis=1, index=None, columns=None, inplace=True)
 
-def drop_any():
-    pass
