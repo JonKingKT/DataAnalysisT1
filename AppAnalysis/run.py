c8513216dc669ca7eed3902c5c3eb4128c7e6f1b
JongKingKT <674958357@qq.com>
2019-06-11 17:30:19 +0800

    结果代码

--- AppAnalysis/run.py
+++ AppAnalysis/run.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import advance
 
+
 # inputfile = open('./data/app_events.csv', 'rb')   #可打开含有中文的地址
 # data = pd.read_csv(inputfile, encoding='gbk' ,iterator=True)
 
@@ -42,37 +43,40 @@ if __name__ == '__main__':
     time_start = time.time()
 
     # 数据地址
-    app_events = readfFile('./data/app_events.csv')
+    # app_events = readfFile('./data/app_events.csv')
     # app_labels = readfFile('./data/app_labels.csv')
     events = readfFile('./data/events.csv')
-    # gender_age_test = readfFile('./data/gender_age_test.csv')
-    gender_age_train = readfFile('./data/gender_age_train.csv')
+    gender_age_test = readfFile('./data/gender_age_test.csv')
+    # gender_age_train = readfFile('./data/gender_age_train.csv')
     # label_categories = readfFile('./data/label_categories.csv')
     # phone_brand_device_model = readfFile('./data/phone_brand_device_model.csv')
 
 
-    events.longitude[events.longitude < 0] = 0
-    events = events[-events.longitude.isin([0])]
-    # events[i].ix[events[i]['longitude'] == '0',['longitude']] = ''
-    allAdvance(events)
+
+    # events.longitude[events.longitude < 0] = 0
+    # events = events[-events.longitude.isin([0])]
+    # # events[i].ix[events[i]['longitude'] == '0',['longitude']] = ''
+    # allAdvance(events)
 
 
 
-    allAdvance(gender_age_train)
-    # gender_age_train = gender_age_train[gender_age_train.gender.isin['M','F']]
-    gender_age_train.age[gender_age_train.age < 0] = 0
-    gender_age_train.age[gender_age_train.age > 70] = 0
-    gender_age_train = gender_age_train[-gender_age_train.age.isin([0])]
+    # allAdvance(gender_age_train)
+    # # gender_age_train = gender_age_train[gender_age_train.gender.isin['M','F']]
+    # gender_age_train.age[gender_age_train.age < 0] = 0
+    # gender_age_train.age[gender_age_train.age > 70] = 0
+    # gender_age_train = gender_age_train[-gender_age_train.age.isin([0])]
 
-    device_id = gender_age_train['device_id'].tolist()
-    events_device = events[['device_id','event_id']]
-    # print(events_device.describe())
-    events_device = events_device[events_device.device_id.isin(device_id)]
-    events_device = events_device.dropna()
-    events_device = events_device['event_id']
-    events_device.to_csv('events_device.csv')
-    events_device_list = events_device.tolist()
-    # print(events_device_list)
+    # allAdvance(gender_age_test)
+    #
+    # device_id = gender_age_test['device_id'].tolist()
+    # # events_device = events[['device_id','event_id']]
+    # # print(events_device.describe())
+    # events_device = events[-events.device_id.isin(device_id)]
+    # events_device = events_device.dropna()
+    # # events_device = events_device[['device_id','event_id']]
+    # events_device.to_csv('events_device_test.csv')
+    # # events_device_list = events_device.tolist()
+    # # print(events_device_list)
 
 
 
@@ -85,18 +89,52 @@ if __name__ == '__main__':
     # new_events.to_csv('new_events.csv', index = True)
     # print(new_events.head())
 
+    # *********************合并主表与手机型号表********************
+    # phone = readfFile('new_phone_brand_device_model.csv')
+    # event = readfFile('events_device_test.csv')
+    # events_device_test = pd.merge(event,phone,on='device_id')
+    # events_device_test.to_csv('events_device_test_phone.csv')
 
-    # 合并主表与手机型号表
-    # allAdvance(phone_brand_device_model)
-    # phone_brand_device_model = phone_brand_device_model[-phone_brand_device_model.device_model.str.contains('???')]
-    # phone_brand_device_model = phone_brand_device_model[['device_id','device_model']]
-    # print(phone_brand_device_model.head(10))
-    # new_events = readfFile('new_events.csv')
-    # new_events = pd.merge(new_events,phone_brand_device_model)
-    # new_events.to_csv('new_events1.csv')
+    # *********************获取app类型********************
+    # app_labels = allAdvance(app_labels)
+    # label_categories = allAdvance(label_categories)
+    # new_label_categories = pd.merge(app_labels, label_categories,on='label_id')
+    # new_label_categories.to_csv('new_label_categories.csv')
+
+    # *********************app类型整合********************
+    # new_label_categories = readfFile('./new_label_categories.csv')
+    # new_label_categories = new_label_categories[['app_id','category']].groupby('app_id')
+    # new_label_categories_dict = {'app_id': "category"}
+    # for i, group in new_label_categories:
+    #     new_label_categories_dict[i] = list(group.category)
+    # new_label_categories = pd.DataFrame.from_dict(new_label_categories_dict, orient='index')
+    # new_label_categories.to_csv('new_label_categories.csv')
+
+    # *********************events_app与app类型整合********************
+    # app_events_train = readfFile('./app_events_train.csv')
+    # new_label_categories = readfFile('./new_label_categories.csv')
+    # categories_list_dict = {'event_id': "category"}
+    # allAdvance(new_label_categories)
+    # for i in range(len(app_events_train)):
+    #     app_events_train_list = app_events_train.loc[i]
+    #     app_id_list_small = []
+    #     app_id_list = app_events_train_list.app_id.split('[')[1].split(',')
+    #     for j in app_id_list:
+    #         # categories_list = new_label_categories[new_label_categories.app_id.isin([int(j)])]
+    #         try:
+    #             categories_list = (new_label_categories.loc[str(new_label_categories.app_id) == j])
+    #             app_id_list_small.extend(categories_list)
+    #         except Exception as e:
+    #             categories_list = []
+    #     app_id_list_small = list(set(app_id_list_small))
+    #     categories_list_dict[app_events_train_list.event_id] = app_id_list_small
+    #
+    # app_events_train = pd.DataFrame.from_dict(categories_list_dict, orient='index')
+    # app_events_train.to_csv('new_app_events_train.csv')
+    # categories_list_dict.clear()
 
 
-
+    # *********************不知名代码********************
     # for i in range(len(app_events)):
     #     app_events[i] = advance.delCols(app_events[i],['is_installed','is_active'])
     #     app_events[i] = advance.drop_nan(app_events[i])
@@ -110,22 +148,34 @@ if __name__ == '__main__':
 
     # *********************合并event_id_app_id********************
     # print(events.iat[0, 0])
-    event_id_app_id_dict={'event_id':"app_id"}
-    app_id_list=[]
+    # event_id_app_id_dict={'event_id':"app_id"}
+    # app_id_list=[]
 
 
     # print(len(events['event_id'].tolist()))
-    for i in range(len(app_events)):
-        app_events[i] = app_events[i][['event_id', 'app_id']]
-        app_events[i] = app_events[i].groupby('event_id')
+    # for i in range(len(app_events)):
+    #     app_events[i] = app_events[i][['event_id', 'app_id']]
+    #     app_events[i] = app_events[i].groupby('event_id')
 
-    for j in range(len(app_events)):
-        event_id_app_id_dict = {'event_id': "app_id"}
-        for i,group in app_events[j]:
-            event_id_app_id_dict[str(i)] = list(group)
-        new_app_events = pd.DataFrame.from_dict(event_id_app_id_dict, orient='index')
-        new_app_events.to_csv('new_app_events'+str(j+1)+'.csv')
-        event_id_app_id_dict.clear()
+    # for i,group in app_events[0]:
+    #     event_id_app_id_dict[i] = list(group)
+    #     print(list(group.app_id))
+    #     print(event_id_app_id_dict[i])
+    # event_id_app_id_dict = {'event_id': "app_id"}
+    # for i, group in app_events[5]:
+    #     event_id_app_id_dict[str(i)] = list(group.app_id)
+    #     # print(list(group))
+    # new_app_events = pd.DataFrame.from_dict(event_id_app_id_dict, orient='index')
+    # new_app_events.to_csv('new_app_events' + str(5 + 1) + '.csv')
+    # event_id_app_id_dict.clear()
+    # for j in range(6,len(app_events)):
+    #     event_id_app_id_dict = {'event_id': "app_id"}
+    #     for i,group in app_events[j]:
+    #         event_id_app_id_dict[str(i)] = list(group.app_id)
+    #         # print(list(group))
+    #     new_app_events = pd.DataFrame.from_dict(event_id_app_id_dict, orient='index')
+    #     new_app_events.to_csv('new_app_events'+str(j+1)+'.csv')
+    #     event_id_app_id_dict.clear()
             # print(app_id_list.append(list(group)))
             # print(i)
             # print(list(group.app_id))
---
+++ AppAnalysis/test1_model.py
@@ -0,0 +1,63 @@
+import time
+
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import fowlkes_mallows_score
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import calinski_harabaz_score
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+import run
+
+
+def float_col(r):
+    return int(time.mktime(r.timetuple())/1000)
+
+
+if __name__ == '__main__':
+    time_start = time.time()
+    train_test1 = run.readfFile('train_test1_rewr.csv')
+
+    test1_data = train_test1[['device_id','event_id','timestamp','longitude','latitude','brand_model_label_code']]
+
+    test1_data['timestamp'] = pd.to_datetime(test1_data['timestamp'])
+    test1_data['timestamp'] = test1_data['timestamp'].apply(lambda r: float_col(r))
+    test1_data = test1_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+    print(test1_data.head())
+    print('pass')
+    test1_age_label =train_test1['age']
+    test1_male_label =train_test1['gender']
+    train_test1_age_data, test_test1_age_data, train_test1_age_label, test_test1_age_label = train_test_split(test1_data, test1_age_label, test_size=0.2,
+                                                                                random_state=42)
+    train_test1_male_data, test_test1_male_data, train_test1_male_label, teat_test1_male_label = train_test_split(test1_data, test1_male_label, test_size=0.2,
+                                                                                random_state=42)
+
+    svm_age = SVC(C = 10, kernel='rbf', gamma=20, decision_function_shape='ovr').fit(train_test1_age_data, train_test1_age_label)
+    print('建立的 AGE_SVM 模型为：\n', svm_age)
+    pred_test1_age_label = svm_age.predict(test_test1_age_data)
+    print('age测试集的预测结果为：\n', pred_test1_age_label)
+    print('AGE_SVM 模型分类报告：\n', classification_report(test_test1_age_label, pred_test1_age_label))
+
+    svm_gender = SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr').fit(train_test1_male_data, train_test1_male_label)
+    print('建立的 gender_SVM 模型为：\n', svm_gender)
+    pred_test1_male_label = svm_gender.predict(test_test1_male_data)
+    print('gender测试集的预测结果为：\n', pred_test1_male_label)
+    print('gender_SVM 模型分类报告：\n', classification_report(teat_test1_male_label, pred_test1_male_label))
+    pred_test1_age_label = pd.DataFrame(pred_test1_age_label)
+    pred_test1_age_label .to_csv('test_test1_age_label.csv')
+    pred_test1_male_label = pd.DataFrame(pred_test1_male_label)
+    pred_test1_male_label.to_csv('pred_test1_male_label.csv')
+
+    try:
+        pickle_file = open('report_age.txt', 'wb')
+        pickle.dump(classification_report(test_test1_age_label, pred_test1_age_label), pickle_file)
+        pickle_file.close()
+    except Exception as e:
+        print(e)
+
+    time_end = time.time()
+    print('程序运行时间为：',time_end - time_start)
